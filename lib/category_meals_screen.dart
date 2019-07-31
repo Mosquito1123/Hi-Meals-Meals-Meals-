03c1a825d5249b91a23539d47d1593565742dc43
Emad Saeed <bollika21@gmail.com>
2019-07-31 20:39:38 +0200

    get filtered meals that are in that selected pressed category

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 import './models/category.dart';
 import 'dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
+
   @override
   Widget build(BuildContext context) {
     final _routeArgs =
         ModalRoute.of(context).settings.arguments as Map<String, String>;
-    print(_routeArgs);
+//    print(_routeArgs);
 
     final Category cat = DUMMY_CATEGORIES
         .where((element) => element.id == _routeArgs['id'])
         .toList()[0];
+
+    final List<Meal> _categoryMeals =
+        DUMMY_MEALS.where((meal) => meal.categories.contains(cat.id)).toList();
+//    print(_categoryMeals.length);
 
     return Scaffold(
       appBar: AppBar(
@@ -21,7 +27,17 @@ class CategoryMealsScreen extends StatelessWidget {
         title: Text(cat.title),
       ),
       body: Center(
-        child: Text('category recipes for category!'),
+        child: ListView.builder(
+            itemCount: _categoryMeals.length,
+            itemBuilder: (ctx, index) {
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    Text(_categoryMeals[index].title),
+                  ],
+                ),
+              );
+            }),
       ),
     );
   }
