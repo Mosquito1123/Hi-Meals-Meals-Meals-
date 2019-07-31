6f4397b3135454fa105c7bde4c78dfc041d47ddd
Emad Saeed <bollika21@gmail.com>
2019-07-31 05:43:51 +0200

    added category item box

---
+++ lib/category_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+class CategoryItme extends StatelessWidget {
+  final Category cat;
+
+  CategoryItme(this.cat);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15),
+      child: Text(cat.title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [cat.color.withOpacity(0.7), cat.color],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight),
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
