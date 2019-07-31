4348fb5861afc53feca0d4b46db20b3a559da0c0
Emad Saeed <bollika21@gmail.com>
2019-07-31 05:47:29 +0200

    mapped dummy data in categories screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
+import './category_item.dart';
+import './dummy_data.dart';
+
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      children: <Widget>[
-        Center(child: Text('this is emad')),
-      ],
+      children: DUMMY_CATEGORIES.map((cat) => CategoryItme(cat)).toList(),
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
