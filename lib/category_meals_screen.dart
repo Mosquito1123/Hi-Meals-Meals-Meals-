ee5ca5ef1aad05b0e88542ab14a5c48b136ccb48
Emad Saeed <3m4d54@gmail.com>
2019-07-31 17:35:55 +0200

    designed a single category page

---
+++ lib/category_meals_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Place category title here'),
+      ),
+      body: Center(
+        child: Text('category recipes for category!'),
+      ),
+    );
+  }
+}
