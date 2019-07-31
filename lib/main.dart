205b00ac6b5c86f6cbba28e33acc9ef26b5356c7
Emad Saeed <bollika21@gmail.com>
2019-07-31 14:06:03 +0200

    add general theming data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import './categories_screen.dart';
 
 void main() => runApp(MyApp());
@@ -8,7 +9,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals App',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              title: TextStyle(
+                fontSize: 24,
+                fontFamily: 'RobotoCondensed',
+              ),
+            ),
+      ),
       home: CategoriesScreen(),
     );
   }
