e017f36635bd4c37dc531b6cd18494236660ed0f
Emad Saeed <bollika21@gmail.com>
2019-08-01 02:50:46 +0200

    removed scaffold and appBar from categories screen since it is used as a tab not a separate page, also with favorites page-removed scaffold

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/category_item.dart';
 import '../dummy_data.dart';
+import '../widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Meals App')),
-      body: GridView(
-        padding: EdgeInsets.all(25),
-        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
+    return GridView(
+      padding: EdgeInsets.all(25),
+      children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
     );
   }
--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -3,11 +3,9 @@ import 'package:flutter/material.dart';
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Favorites goes here'),
-        ),
+    return Container(
+      child: Center(
+        child: Text('Favorites goes here'),
       ),
     );
   }
