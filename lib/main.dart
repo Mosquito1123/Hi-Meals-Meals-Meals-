b18b092a9b225c82946ac698cde5c2798699abf7
Emad Saeed <bollika21@gmail.com>
2019-08-01 03:30:57 +0200

    added filters screen page and registered its route

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './screens/categories_screen.dart';
+import './screens/filters_screen.dart';
 import './screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/tabs_screen.dart';
@@ -32,6 +33,7 @@ class MyApp extends StatelessWidget {
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       onGenerateRoute: (settings) => null,
       onUnknownRoute: (settings) => MaterialPageRoute(
---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = '/filter';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Filters page'),
+    );
+  }
+}
