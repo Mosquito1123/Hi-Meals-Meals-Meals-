637e335a85d128fd2fb790c378f0deadae161a29
Emad Saeed <bollika21@gmail.com>
2019-08-01 04:06:59 +0200

    add drawer to fliters page

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filter';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: MainDrawer(),
       appBar: AppBar(
         title: Text('Filters'),
       ),
