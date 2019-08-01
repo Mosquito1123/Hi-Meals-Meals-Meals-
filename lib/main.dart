3b943c8c88a47d44859240283324ac3b64f78bf5
Emad Saeed <bollika21@gmail.com>
2019-08-01 02:47:58 +0200

    add tabs + favorites tab + used them

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
 import 'screens/category_meals_screen.dart';
+import 'screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -26,7 +27,8 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+//      home: CategoriesScreen(),
+      home: TabsScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+import './favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {'title': 'Categories', 'page': CategoriesScreen()},
+    {'title': 'Your Favorites', 'page': FavoritesScreen()}
+  ];
+  int _selectPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectPageIndex]['title']),
+      ),
+      body: _pages[_selectPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectPageIndex,
+        type: BottomNavigationBarType.shifting,
+        items: [
+          BottomNavigationBarItem(
+            // only add bg color in tabs with BottomNavigationBarType.shifting
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            // only add bg color in tabs with BottomNavigationBarType.shifting
+            backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            title: Text('Favorites'),
+          ),
+        ],
+      ),
+    );
+  }
+}
